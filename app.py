3be73f9d2fda6dbdcb051ec68d8f66945e1d0f5c
kartikeymalvi <kartikeymalviya02@gmail.com>
2026-09-10 13:08:32 +0530

    updated new relese 3.9

--- app.py
+++ app.py
@@ -10,9 +10,9 @@ CORS(app)
 database.init_db()
 
 # Server App Version Config
-CURRENT_VERSION = "3.7"
+CURRENT_VERSION = "3.9"
 MANDATORY_UPDATE = False
-RELEASE_NOTES = "1. Delivery OTP & Tracking Extractor upgrade with multi-unit bulk extraction\n2. Address Manager preset fields locked to read-only except Mobile Number"
+RELEASE_NOTES = "1.updated invoice downloading /n 2.updated auto order placement for RTGS  "
 
 UPDATES_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "static", "updates")
 os.makedirs(UPDATES_DIR, exist_ok=True)
